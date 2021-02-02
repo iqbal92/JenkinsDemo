1041de3dc924053e2e65a96d21433da5f4767814
iqbal92 <ikbal.pixster@gmail.com>
2021-02-02 10:50:01 +0530

    Add files via upload

---
+++ JenkinsDemoProject/JenkinsDemoProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JenkinsDemoProject
+//
+//  Created by Pixster Studio on 02/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world")
+    }
+
+
+}
+
